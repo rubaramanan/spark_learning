bcaceb2a60ee518eff7661e2761e26d9265b8af4
Ramanan <rubaramanan1997@gmail.com>
2025-02-25 05:58:35 -0500

    ratings histogram

---
+++ src/RDD/ratings_histogram.py
@@ -0,0 +1,14 @@
+from pyspark import SparkContext, SparkConf
+from collections import OrderedDict
+
+conf = SparkConf().setMaster('spark://spark-master:7077').setAppName('Rating Histogram')
+sc = SparkContext(conf=conf)
+
+file = sc.textFile('/opt/bitnami/spark/data/u.data')
+
+ratings = file.map(lambda s: s.split()[2])
+count = ratings.countByValue()
+
+or_dic = OrderedDict(sorted(count.items(), reverse=True))
+for k,v in or_dic.items():
+    print(k, v)
