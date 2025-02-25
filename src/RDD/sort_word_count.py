eb2edbd78289616b102d0094178a3fa903e71326
Ramanan <rubaramanan1997@gmail.com>
2025-02-25 06:50:23 -0500

    sort word count

--- src/RDD/sort_word_count.py
+++ src/RDD/sort_word_count.py
@@ -1,18 +1,20 @@
-from pyspark import SparkConf, SparkContext
 import re
-from collections import OrderedDict
+
+from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster('spark://spark-master:7077').setAppName('WordCount')
 sc = SparkContext(conf=conf)
 
 file = sc.textFile('/opt/bitnami/spark/data/Book')
 
+
 def data_cleaning(text: str):
     return re.compile(r'\W+', re.UNICODE).split(text.lower())
 
+
 line = file.flatMap(data_cleaning)
-line_c = line.map(lambda x: (x,1))
-word_c = line_c.reduceByKey(lambda x,y: x+y)
+line_c = line.map(lambda x: (x, 1))
+word_c = line_c.reduceByKey(lambda x, y: x + y)
 # word_c = line.countByValue() # returns default dict
 sorted_word = word_c.map(lambda x: (x[1], x[0])).sortByKey()
 
@@ -21,5 +23,5 @@ sorted_word = word_c.map(lambda x: (x[1], x[0])).sortByKey()
 #         print(clean_word, c)
 
 for i in sorted_word.collect():
-    if clean_word:=i[1].encode('ascii', 'ignore'):
+    if clean_word := i[1].encode('ascii', 'ignore'):
         print(clean_word, i[0])
