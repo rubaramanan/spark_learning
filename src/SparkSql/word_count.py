cac826843593fbe29e037e6740eb81a2b28ace98
Ramanan <rubaramanan1997@gmail.com>
2025-03-15 05:18:10 -0400

    word cound with dataframe

--- src/SparkSql/word_count.py
+++ src/SparkSql/word_count.py
@@ -1,7 +1,24 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+
 
 session = SparkSession.builder.appName('word count').getOrCreate()
 
-words = session.read.text('/opt/bitnami/spark/data/Book')
+df = session.read.text('/opt/bitnami/spark/data/Book')
 
-words.selectExpr("split(value, ' ') as word").show()
+# words.selectExpr("split(value, ' ') as word").show()
+
+# explode function exact like flatmap
+words = df.select(func.explode(func.split(df.value, '\\W+')).alias('word'))
+wordsWithoutEmptyString = words.filter(words.word != "")
+
+wordsLower = wordsWithoutEmptyString.select(func.lower(wordsWithoutEmptyString.word).alias('word'))
+count = wordsLower.groupBy('word').count()
+countSort = count.sort('count')
+countSort.show()
+
+#it will show all data
+countSort.show(countSort.count())
+# wordsLower.show()
+
+session.stop()
